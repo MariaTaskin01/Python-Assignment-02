e94c358b30ad6da4f689ab5f95ff3c4fb2f7945d
maria63 <mariataskinaumanee2001@gmail.com>
2025-06-26 23:49:57 +0600

    First Commit

---
+++ Operators/Arithmetic Operators.py
@@ -0,0 +1,45 @@
+# 1. Calculate the area and perimeter of a rectangle (length × width).
+
+print("1. Calculate the area and perimeter of a rectangle (length × width).")
+length = 20
+breadth = 60
+
+print("Length = ", length)
+print("Breadth = ", breadth)
+
+area = length * breadth
+print('Area of a rectangle:', area)
+
+# 2. Convert temperature from Celsius to Fahrenheit.
+
+print("2. Convert temperature from Celsius to Fahrenheit.")
+C = 20
+print("Temperature in Celsius = ", C)
+
+F = (C * 9/5) + 32
+print('Temperature in Fahrenheit:', F)
+
+# 3. Find the remainder of 25 divided by
+
+print("3. Find the remainder of 25 divided by 5")
+a = 25
+print("a = ", a)
+
+b = a % 5
+print("Remainder of a = ", b)
+
+# 4. Compute the result of 2³ × 5 + 0
+
+print("4. Compute the result of 2³ × 5 + 0")
+
+a = (2**3 * 5) + 0
+
+print("Result: 2³ × 5 + 0 = ", a)
+
+# 5. Calculate the average of 5 numbers.
+
+print("5. Calculate the average of 5 numbers.")
+a = [4,5,6.20,45]
+
+average = sum(a) / len(a)
+print("Average of 5 numbers = ", average )
