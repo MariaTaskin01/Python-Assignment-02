ca58ca174d0cf6bcffdb749ee0c32ed3e2645124
maria63 <mariataskinaumanee2001@gmail.com>
2025-06-27 04:27:33 +0600

    Third Commit

---
+++ Operators/Assignment Operators.py
@@ -0,0 +1,25 @@
+# 1. Start with a variable `x = 0` and apply `+=`, `-=`, `*=`, `/=`, `%=`, step by step.
+
+print("1. Start with a variable `x = 0` and apply `+=`, `-=`, `*=`, `/=`, `%=`, step by step.")
+
+x = 0
+print("Initial value of x:", x)
+
+x += 5
+print("x += 5:", x)
+
+x -= 3
+print("x -= 3:", x)
+
+x *= 2
+print("x *= 2:", x)
+
+x /= 4
+print("x /= 4:", x)
+
+x %= 3
+print("x %= 3:", x)
+
+# 2. Create a counter that increases by 2 on each step, using `+=`.
+# 3. Create a simple savings balance calculator: add interest using `*=`.
+# 4. Simulate a countdown where each step decreases by  using `-=`.
