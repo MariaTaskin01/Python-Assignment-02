9ab813a35461d0ae3d4e7164b2cc11a4aaad352f
maria63 <mariataskinaumanee2001@gmail.com>
2025-06-27 08:05:02 +0600

    Assignment Operators

---
+++ Operators/Comparison Operators.py
@@ -0,0 +1,42 @@
+# 1. Check if two numbers are equal.
+
+print("1. Check if two numbers are equal.")
+a = 10
+b = 40
+
+print("a =", a)
+print("b =", b)
+
+if a == b:
+    print("The two numbers are equal")
+else:
+    print("The two numbers are not equal")
+
+# 2. Compare the ages of two people and print who is older.
+
+print("2. Compare the ages of two people and print who is older.")
+age_1 = 10
+age_2 = 40
+
+print("age_1 =", age_1)
+print("age_2 =", age_2)
+
+if age_1 > age_2:
+    print("age_1 is older than age_2")
+else:
+    print("age_2 is older than age_1")
+
+# 3. Write a program to determine if a year is before or after 2000.
+
+print("3. Write a program to determine if a year is before or after 2000.")
+year = 2025
+
+print("year =", year)
+
+if year > 2000:
+    print("This year is after 2000")
+else:
+    print("This year is before 2000")
+# 4. Take two numbers from the user and check which one is greater.
+# 5. Compare marks of two students to check if both got the same score.
+# 6. Check if a number is not equal to zero before dividing.
